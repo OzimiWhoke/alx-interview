941b56507050363fecea6d069d0efe5de557d85d
OzimiWhoke <ozimi11zz@gmail.com>
2023-07-27 23:08:07 +0000

    pascal triangle

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -5,16 +5,26 @@
 '''
 
 
-def pascal_triangle(num):
-    ''' function that returns a pascal triangle '''
-    lst = []
-    if num <= 0:
-        return lst
-    for i in range(num):
-        lst.append([])
-        lst[i].append(1)
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            lst[i].append(lst[i - 1][j - 1] + lst[i - 1][j])
-        if i > 0:
-            lst[i].append(1)
-    return lst
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle

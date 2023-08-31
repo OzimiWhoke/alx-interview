a1a3b5f2e96ef0e5880c6f86f62b70c5021abba0
OzimiWhoke <ozimi11zz@gmail.com>
2023-08-31 20:16:22 +0000

    NQueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+""" N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard """
+import sys
+
+
+def is_safe(board, row, col, N):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens(board, col, N):
+    if col >= N:
+        print([[row, col] for row, val in enumerate(board) if val == 1])
+        return True
+
+    res = False
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1, N) or res
+            board[i][col] = 0
+    return res
+
+
+def nqueens(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens(board, 0, N)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    nqueens(sys.argv[1])
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,3 @@
+# NQueens
+
+The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.
